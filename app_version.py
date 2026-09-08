7838ea0de3780983bb66210375ae0cfc91404e75
Paulo Santesso <paulosantesso51@gmail.com>
2026-09-08 20:17:57 -0300

    Release 1.0.8

--- app_version.py
+++ app_version.py
@@ -1,7 +1,7 @@
 """Release identity shared by the application, installer and updater."""
 
 APP_NAME = "Accessible Reels"
-APP_VERSION = "1.0.7"
+APP_VERSION = "1.0.8"
 GITHUB_REPOSITORY = "paulosantesso1/accessible-reels"
 SETUP_ASSET_NAME = "Accessible-Reels-Setup.exe"
 SETUP_CHECKSUM_ASSET_NAME = f"{SETUP_ASSET_NAME}.sha256"
